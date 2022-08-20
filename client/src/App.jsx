67a8b541566fd5cfb2214ac1b1092df83d1d9d75
CREDO23 <thierrybakera12@gmail.com>
2022-08-20 14:19:51 +0200

    feat : configuration tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1 class="text-3xl text-red-700  font-bold ">
+    Hello world!
+  </h1>
+    </div>
+  )
+}
+
+export default App
